99e321b57198164aaa8288643ae316b617f7fa68
ybbt <olexiy.k@i.ua>
2021-12-10 17:53:00 +0200

    form of registration

--- pages/register.js
+++ pages/register.js
@@ -1,50 +1,73 @@
 import { Alert } from "antd";
+import "antd/dist/antd.css";
 
-// *********************** Formik
-import { Formik, Form, Field, ErrorMessage } from "formik";
-// ***********************
-
-// *********************** yup
+import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
-// ***************************
 
-export default function Register() {
-    function handleSave({ postContent }, { resetForm }) {
-        onSave(postContent);
-        resetForm();
-    }
+import axiosInstance from "../libs/axiosInstance";
 
-    const PostSaveSchema = Yup.object().shape({
-        postContent: Yup.string().required("Required Field!"),
-    });
+const SignupSchema = Yup.object().shape({
+    firstName: Yup.string()
+        .min(2, "Too Short!")
+        .max(50, "Too Long!")
+        .required("Required"),
+    password: Yup.string()
+        .min(8, "Too Short!")
+        .max(50, "Too Long!")
+        .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+});
 
-    function madeForm({ errors, touched }) {
-        const errorField = errors.postContent ? (
-            <Alert message={errors.postContent} type="error" showIcon />
-        ) : null;
+export default function ValidationSchemaExample() {
+    async function handleSubmitData({ firstName, email, password }) {
+        // console.log(values, "values");
 
-        return (
-            <Form>
-                <Field as="textarea" id="postContent" name="postContent" />
-                {errorField}
-                <div>
-                    <button type="submit">button</button>
-                </div>
-            </Form>
-        );
+        try {
+            const response = await axiosInstance.post("/register", {
+                name: firstName,
+                email,
+                password,
+            });
+
+            // setPosts([...posts, response.data.data]);
+        } catch (error) {
+            message.error(
+                `${error.response.data.message} - ${error.response.data.errors.text[0]}`
+            );
+            console.log(error.response);
+        }
     }
 
     return (
-        <>
+        <div>
+            <h1>Signup</h1>
             <Formik
-                initialValues={""}
-                validateOnChange={!!editContent}
-                validateOnBlur={!!editContent}
-                validationSchema={PostSaveSchema}
-                onSubmit={handleSave}
+                initialValues={{
+                    firstName: "",
+                    email: "",
+                    password: "",
+                }}
+                validationSchema={SignupSchema}
+                onSubmit={handleSubmitData}
             >
-                {madeForm}
+                {({ errors, touched }) => (
+                    <Form>
+                        <Field name="firstName" />
+                        {errors.firstName && touched.firstName ? (
+                            <div>{errors.firstName}</div>
+                        ) : null}
+                        <Field name="email" type="email" />
+                        {errors.email && touched.email ? (
+                            <div>{errors.email}</div>
+                        ) : null}
+                        <Field type="password" name="password" />
+                        {errors.password && touched.password ? (
+                            <div>{errors.password}</div>
+                        ) : null}
+                        <button type="submit">Submit</button>
+                    </Form>
+                )}
             </Formik>
-        </>
+        </div>
     );
 }
