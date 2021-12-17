702069f694f5cde2fe4977eaedf7cb94f6efdbd3
ybbt <olexiy.k@i.ua>
2021-12-17 17:58:23 +0200

    Недопереоформлена форма региістрації

--- pages/register.js
+++ pages/register.js
@@ -7,6 +7,7 @@ import * as Yup from "yup";
 import Cookies from "js-cookie";
 
 import Router from "next/router";
+import classNames from "classnames";
 
 import axiosInstance from "../libs/axiosInstance";
 
@@ -63,6 +64,52 @@ export default function Register() {
         }
     }
 
+    function madeForm({ errors, touched }) {
+        const fieldStyle = classNames("border-[#949494] border-2");
+
+        //! Допрацювати
+        function errorField(errors, touched, name) {
+            alert();
+            return (
+                <>
+                    {errors[name] && touched[name] && (
+                        <Alert message={errors[name]} type="error" showIcon />
+                    )}
+                </>
+            );
+        }
+
+        const errorUser = errorField(errors, touched, "userName");
+
+        return (
+            <Form>
+                <div className="flex flex-col">
+                    <Field name="userName" className={fieldStyle} />
+                    {errorField}
+                    <Field name="email" type="email" className={fieldStyle} />
+                    {errors.email && touched.email ? (
+                        <Alert message={errors.email} type="error" showIcon />
+                    ) : null}
+                    <Field
+                        type="password"
+                        name="password"
+                        className={fieldStyle}
+                    />
+                    {errors.password && touched.password ? (
+                        <Alert
+                            message={errors.password}
+                            type="error"
+                            showIcon
+                        />
+                    ) : null}
+                    <button type="submit" className="bg-[#54C1FF] text-white">
+                        Submit
+                    </button>
+                </div>
+            </Form>
+        );
+    }
+
     return (
         <div className="w-full h-screen flex justify-center items-center flex-col">
             <h1>Sign Up</h1>
@@ -75,7 +122,7 @@ export default function Register() {
                 validationSchema={SignupSchema}
                 onSubmit={handleSubmitData}
             >
-                {({ errors, touched }) => (
+                {/* {({ errors, touched }) => (
                     <Form>
                         <div className="flex flex-col">
                             <Field
@@ -121,7 +168,8 @@ export default function Register() {
                             </button>
                         </div>
                     </Form>
-                )}
+                )} */}
+                {madeForm}
             </Formik>
         </div>
     );
