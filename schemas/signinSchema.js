1dff1d33e1b64c833e373e6ff874dca55ae26ae0
ybbt <olexiy.k@i.ua>
2022-01-07 10:35:22 +0200

    update yup-schema in login

--- schemas/signinSchema.js
+++ schemas/signinSchema.js
@@ -2,19 +2,22 @@ import * as Yup from "yup";
 
 const signinSchema = Yup.object().shape({
     login: Yup.lazy((value) => {
-        switch (value.includes("@")) {
-            case true:
-                return Yup.string()
-                    .email("Invalid email")
-                    .required("Required email");
-            case false:
-                return Yup.string()
-                    .required("Required user name")
-                    .matches(
-                        /^[a-zA-Z0-9]+$/,
-                        "User name can only contain letters and numbers"
-                    );
+        if (value) {
+            switch (value.includes("@")) {
+                case true:
+                    return Yup.string()
+                        .email("Invalid email")
+                        .required("Required email");
+                case false:
+                    return Yup.string()
+                        .required("Required user name")
+                        .matches(
+                            /^[a-zA-Z0-9]+$/,
+                            "User name can only contain letters and numbers"
+                        );
+            }
         }
+        return Yup.string().required("Required field");
     }),
     password: Yup.string()
         .required("No password provided")
