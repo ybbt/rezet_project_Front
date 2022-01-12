ce8ff7c77947ea0bb1133484a0e1b5be87926bef
ybbt <olexiy.k@i.ua>
2022-01-12 17:58:13 +0200

    знову налаштував Luxon

--- hooks/useFormatDate.js
+++ hooks/useFormatDate.js
@@ -2,13 +2,12 @@ import { DateTime } from "luxon";
 import { useMemo } from "react";
 
 export default function useFormatDate(date, format) {
-    console.log(date, "not date in useFormatDate");
     if (!date) {
         return null;
     }
-    console.log(
-        new DateTime(date).toFormat(format),
-        "newDate from useFormatDate"
+
+    return useMemo(
+        () => DateTime.fromISO(date).toFormat(format),
+        [date, format]
     );
-    return useMemo(() => new DateTime(date).toFormat(format), [date, format]);
 }
